b7c5778ce3210239674df3e010e85b689195f579
anusha-peddoju <o180318@rguktong.ac.in>
2023-02-12 22:22:07 +0530

    initial commit

---
+++ form1.js
@@ -0,0 +1,13 @@
+
+
+function validateform()
+{
+    var name=document.myform.fn.value;
+
+// regular expression that checks if message contains 'programming'
+const exp = /^s/;
+
+// check if exp is present in message
+let result = name.match(exp);
+;
+}
